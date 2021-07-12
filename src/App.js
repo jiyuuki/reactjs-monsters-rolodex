deb801b698ed0b87d0dfecd084a01eec131e3b94
EL HAJRAOUI Salma <salma.elhajraoui@docaposte.fr>
2021-07-12 17:44:25 +0200

    monsters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            monsters: [
+                {
+                    name: 'N. Gin',
+                    id: 'n-gin'
+                },
+                {
+                    name: 'Louise',
+                    id: 'louise'
+                },
+                {
+                    name: 'Nitrus Brio',
+                    id: 'nitrus-brio'
+                },
+                {
+                    name: 'Nefarious Tropy',
+                    id: 'nefarious-tropy'
+                },
+                {
+                    name: 'Nefarious Tropy (female)',
+                    id:  'nefarious-tropy-f'
+                },
+                {
+                    name: 'Neo Cortex',
+                    id:  'neo-cortex'
+                }
+            ]
+        }
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    {
+                        this.state.monsters.map((monster) => {
+                            return <h1 key={ monster.key }> { monster.name } </h1>
+                        })
+                    }
+                    <button onClick={() => this.setState({firstName: 'JiYuuki' })}>Click to change the name</button>
+                </header>
+            </div>
+        )
+    }
 }
-
 export default App;
