b60131a5c2d7c294076888be4e85df2a22a1cc03
salmaelhajraoui <salma.hajrawi@gmail.com>
2021-07-27 23:49:08 +0200

    fix call style css on CardList

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Card from '../card/card-component'
 
-import './card-list.style.css'
+import './cardList.style.css'
 
 const CardList = (props) => (
     <div className='App'>
